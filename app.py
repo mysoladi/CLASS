e83da9b9dac2195c9dec03acd3ebf3f8c1b329a0
Myles Soladine <mysoladine@gmail.com>
2022-03-03 08:57:13 -0800

    commiting new stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return 'Hello, Worldasdasdasd!32423423424'
+    return render_template('index.html')
