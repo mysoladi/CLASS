829431a207b0d08bf533f130357409b997ea83e0
Myles Soladine <mysoladine@gmail.com>
2022-03-03 09:42:02 -0800

    new files created

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import imp
 from flask import Flask, render_template
+import csv
+
 app = Flask(__name__)
 
+with open('dinosaurs.csv', 'r') as csvfile:
+    data = csv.Dictreader(csvfile)
+    dinosaurs = {row['Slug']:{'name':row['name'], 'description': row['description'], 'image': row['image'], 'image-credit': row['image-credit'], 'source-url': row['source-url'], 'source-credit': row['source-credit']} for row in data}
+
+
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def hiindex():
+    return render_template('index.html', dinosaurs=dinosaurs)
